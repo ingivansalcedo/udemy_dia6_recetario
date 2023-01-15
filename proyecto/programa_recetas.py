d9f9ad1a93130a50a860ee4e1768be9f8feac62c
Ivan Salcedo <ingivansalcedo@yahoo.com.co>
2023-01-15 09:10:18 -0500

    Funcion numero recetas, retorna cantidad de recetas (.txt) tienen la carpeta RECETAS

--- proyecto/programa_recetas.py
+++ proyecto/programa_recetas.py
@@ -9,7 +9,16 @@ def ruta_acceso():
     ruta = Path(home,carpeta)
     return ruta
 
-print (saludo_bienvenida())
-print (f"Ruta de acceso: {ruta_acceso()}")
-print ("Seleccione alguna de las siguientes opciones: ")
+def numero_recetas(ruta):
+    recetas = 0
+    for txt in Path(ruta).glob('**/*.txt'):
+        recetas += 1
+    return recetas
 
+ruta = ruta_acceso()
+opcion = 1
+
+print (saludo_bienvenida())
+print (f"Ruta de acceso a sus recetas: {ruta}")
+print (f"Cantidad de recetas: {numero_recetas(ruta)}")
+print ("Seleccione alguna de las siguientes opciones: ")
