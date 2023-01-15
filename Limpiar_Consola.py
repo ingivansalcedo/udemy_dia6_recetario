2ad4979eef9b8f201866e0a9077c32d17b800c14
Ivan Salcedo <ingivansalcedo@yahoo.com.co>
2023-01-14 20:06:25 -0500

    Leccion para aprender a limpiar consola importanto la biblioteca system

---
+++ Limpiar_Consola.py
@@ -0,0 +1,9 @@
+from os import system
+from pathlib import Path
+
+# nombre = input("Dime tu nombre: ")
+# edad = input("Dime tu edad: ")
+
+# system('clear')
+
+# print(f"Tu nombre es {nombre} y tienes {edad} años")
